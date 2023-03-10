e61c75f138d0c3dd4d30702e30dfe9bbd27a5876
tkharrat <tarak@realanalytics.co.uk>
2023-03-10 10:48:24 +0000

    fixing typo

--- decrypt_secret.sh
+++ decrypt_secret.sh
@@ -3,7 +3,7 @@
 # Decrypt the file
 mkdir $HOME/secrets
 
-n# --batch to prevent interactive command
+# --batch to prevent interactive command
 # --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$BETTING_SECRET" \
 --output $HOME/secrets/config.toml config.toml.gpg
